94dbd4d39becd2e984676e8e92b9e9fda1516cb0
ProGrammer_365 <133169817+ProCoder36908@users.noreply.github.com>
2024-01-29 20:46:28 -0500

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/static/tiw/',
-    bare:'https://skoolhub.fun/bare/',
+    bare:'https://coca.colaclass.com/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/static/uv/uv.handler.js',
